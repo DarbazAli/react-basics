062f40337d9f17fc70caa0cbc4c67a9a438f8f12
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-07 22:04:14 +0300

    using React.Fragment with Components

--- src/components/StatelessComp.js
+++ src/components/StatelessComp.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const StatelessComp = () => {
     return(
-        <div>
-            This is a stateless functional Component!
-        </div>
+        <Fragment>
+            This is a statelss functional component.
+        </Fragment>
     )
 }
 
