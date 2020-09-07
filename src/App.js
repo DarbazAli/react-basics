73831a5133cf3c9ffd89038467d7f08fd6d0efe0
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-07 20:43:12 +0300

    Complext JSX Element

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import { render } from "react-dom";
 const App = () => {
   return (
     <div>
+      {/* Simple JSX ELEMENT */}
       <h1>Hello JSX!</h1>
+
+      {/* more complext JSX element */}
+      {/* JSX should return one single element */}
+      <div>
+        <h3>New Element</h3>
+        <h3>Another JSX element</h3>
+      </div>
     </div>
   );
 };
